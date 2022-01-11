7f0208637747d7d65018a117f62c179c0c56ef6f
vyralmarketing <getvyral@gmail.com>
2022-01-11 15:34:46 -0600

    mobile button dropdown fix

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,3 +1,13 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
